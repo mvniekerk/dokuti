59bb57fa3a533b199e72623f52bcb7381cf28d53
Melissa Palmer <melissa.palmer@gmail.com>
2020-01-17 09:24:16 +0200

    force a 'code change' as looks like sonarqube only shows % covered on changes for branches (not whole project)

--- src/main/java/za/co/grindrodbank/dokuti/attribute/AttributeServiceImpl.java
+++ src/main/java/za/co/grindrodbank/dokuti/attribute/AttributeServiceImpl.java
@@ -17,62 +17,62 @@ import za.co.grindrodbank.dokuti.exceptions.InvalidRequestException;
 import za.co.grindrodbank.dokuti.exceptions.ResourceNotFoundException;
 import za.co.grindrodbank.security.service.accesstokenpermissions.SecurityContextUtility;
 
-
 @Service
 public class AttributeServiceImpl implements AttributeService {
 
-	@Autowired
-	private AttributeRepository attributeRepository;
+    @Autowired
+    private AttributeRepository attributeRepository;
 
-	@Override
-	public AttributeEntity save(AttributeEntity attribute) {
-		attribute.setUpdatedBy(UUID.fromString(SecurityContextUtility.getUserIdFromJwt()));
+    @Override
+    public AttributeEntity save(AttributeEntity attribute) {
 
-		return attributeRepository.save(attribute);
-	}
+        attribute.setUpdatedBy(UUID.fromString(SecurityContextUtility.getUserIdFromJwt()));
 
-	public AttributeEntity createAttribute(String name, String validationRegex) {
-		AttributeEntity attribute = new AttributeEntity(name, validationRegex);
+        return attributeRepository.save(attribute);
+    }
 
-		if (!attribute.validationRegexIsValid()) {
-			throw new InvalidRequestException("Invalid validation regular expression provided for attribute.", null);
-		}
+    public AttributeEntity createAttribute(String name, String validationRegex) {
+        AttributeEntity attribute = new AttributeEntity(name, validationRegex);
 
-		attribute.setUpdatedBy(UUID.fromString(SecurityContextUtility.getUserIdFromJwt()));
+        if (!attribute.validationRegexIsValid()) {
+            throw new InvalidRequestException("Invalid validation regular expression provided for attribute.", null);
+        }
 
-		try {
-			attribute = attributeRepository.save(attribute);
-		} catch (Exception e) {
-			throw new DatabaseLayerException("Error saving new document attribute.", e);
-		}
+        attribute.setUpdatedBy(UUID.fromString(SecurityContextUtility.getUserIdFromJwt()));
 
-		return attribute;
-	}
+        try {
+            attribute = attributeRepository.save(attribute);
+        } catch (Exception e) {
+            throw new DatabaseLayerException("Error saving new document attribute.", e);
+        }
 
-	public AttributeEntity findById(Short attributeId) {
-		Optional<AttributeEntity> optionalAttribute = attributeRepository.findById(attributeId);
+        return attribute;
+    }
 
-		if (!optionalAttribute.isPresent()) {
-			throw new ResourceNotFoundException("Attribute not found", null);
-		}
+    public AttributeEntity findById(Short attributeId) {
+        Optional<AttributeEntity> optionalAttribute = attributeRepository.findById(attributeId);
 
-		return optionalAttribute.get();
-	}
-	
-	public AttributeEntity updateAttribute(Short attributeId, String name, String validationRegex) {
-		AttributeEntity attributeEntity = findById(attributeId);
-		attributeEntity.setName(name);
-		attributeEntity.setValidationRegex(validationRegex);
+        if (!optionalAttribute.isPresent()) {
+            throw new ResourceNotFoundException("Attribute not found", null);
+        }
 
-		if (!attributeEntity.validationRegexIsValid()) {
-			throw new InvalidRequestException("Invalid validation regular expression provided for attribute.", null);
-		}
+        return optionalAttribute.get();
+    }
 
-		return save(attributeEntity);
-	}
+    public AttributeEntity updateAttribute(Short attributeId, String name, String validationRegex) {
+        AttributeEntity attributeEntity = findById(attributeId);
+        attributeEntity.setName(name);
+        attributeEntity.setValidationRegex(validationRegex);
 
-	public Page<AttributeEntity> findAll(Pageable pageable) {
-		return attributeRepository.findAll(pageable);
-	}
+        if (!attributeEntity.validationRegexIsValid()) {
+            throw new InvalidRequestException("Invalid validation regular expression provided for attribute.", null);
+        }
+
+        return save(attributeEntity);
+    }
+
+    public Page<AttributeEntity> findAll(Pageable pageable) {
+        return attributeRepository.findAll(pageable);
+    }
 
 }
